26a5a6c1e8d4661239681ebaf913163edabac802
ayo-ajayi <ajayiayomide247@gmail.com>
2023-10-08 23:19:51 +0200

    added docs to feed dir

--- feed/main.py
+++ feed/main.py
@@ -1,11 +1,17 @@
+#import the required libraries
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text
 import xmltodict
+
+#create engine and session
 engine = create_engine("sqlite:///data.sqlite")
 session = sessionmaker(bind=engine)()
 
-result = []
+
+#use a try except block to catch any errors that may occur in the session.execute() function
 try:
+    #execute the query using the session object
+
     result = session.execute(
         text(
             """
@@ -30,43 +36,92 @@ try:
     )
 except Exception as e:
     print("sql exec error: "+ str(e))
-session.close()
+
+#close the session
+finally:
+    session.close()
 
 base_url = "https://butopea.com/"
+
+
+if result == []:
+    print("No products found")
+    exit()
+
 products_dict = {}
-result_list = list(result)
-for i in range(len(result_list)):
-    prod_dict = result_list[i]._asdict()
+
+#loop through the array of results and attach the additional image links to the appropriate product id
+for row in result:
+
+    #assert the result into an idiomatic python dictionary
+    prod_dict = row._asdict()
     prod_id = prod_dict["id"]
+
+    #create the product link by appending each product id to the base url
     prod_dict["link"] = base_url +"p/"+ str(prod_id)
+
+    #create the image link by appending each image_link from db to the base url
     prod_dict["image_link"] = base_url + prod_dict["image_link"]
+
+    
+    #if the product id of the row from the database is not already in the products_dict, add it to the products_dict
+    #the product id is the focus and the products_dict is now a key-value pair,
+    # where it's id is the key and the value is the dictionary (prod_dict) that contains additional image links of each product id together in one place
     if prod_id not in products_dict:
         products_dict[prod_id] = prod_dict
+
+        #create an empty array that will hold the additional image links
         products_dict[prod_id]["additional_image_links"] = []
     
+    
     if prod_dict["additional_image_link"]:
+        
+        # if additional_image_link exists for the row in the table, append it to the base_url to form a complete link
         prod_dict["additional_image_link"] = base_url + prod_dict["additional_image_link"]
+
+        #collect the addtional image links and their sort order into the products_dict value that corresponds to the product id
+        #the value is a dictionary that has "additional_image_links" as the key and an array of tuples as the value
         products_dict[prod_id]["additional_image_links"].append((prod_dict["additional_image_link"], prod_dict["sort_order"]))
-    if "additional_image_link" in products_dict[prod_id]:
-        del products_dict[prod_id]["additional_image_link"]
-    if "sort_order" in products_dict[prod_id]:
-        del products_dict[prod_id]["sort_order"]
+
+        #delete the additional_image_link and sort_order keys from the products_dict[prod_id] dictionary since the values are already read
+        products_dict[prod_id].pop("additional_image_link", None)
+        products_dict[prod_id].pop("sort_order", None)
 
 products=[]
+
+#take out all the values from the products_dict and put them in an array (instead of the dictionary that has the product id as the key)
 for prod_id in products_dict:
     if products_dict[prod_id]["additional_image_links"]:
+
+        #sort the additional image links by their sort order
         sorted_links = sorted(products_dict[prod_id]["additional_image_links"], key=lambda x: int(x[1]))
+
+        #create an additional_image_link key and assign it the value of the sorted additional image links
         products_dict[prod_id]["additional_image_link"] = [link[0] for link in sorted_links]
-        del products_dict[prod_id]["additional_image_links"]
-    products.append(products_dict[prod_id])
     
+    #append the values to the products array
+    products.append(products_dict[prod_id])
+
+#free up memory
+del products_dict
+del result
+
+
+#save the products to a an xml file
 try:
     feed = {"feed":{
         "product": products
     }}
+
+    #serialize the feed object to xml
     xml_data = xmltodict.unparse(feed, pretty=True)
+
+    #open a file for writing with utf-8 encoding because of special characters
     f= open("feed.xml", "w", encoding="utf-8")
     f.write(xml_data)
     print("XML file created successfully")
+
+    #close the file
+    f.close()
 except Exception as e:
     print("dict to xml error: "+ str(e))
---
+++ feed/readme.md
@@ -0,0 +1,92 @@
+# Documentation: Product Feed (XML) from Database
+
+This readme explains how to fetch product data from an SQLite database, process the results, and save the structured data into an XML file.
+
+## Prerequisites
+
+- Ensure you have `python` installed on your system.
+
+## Setup and Execution
+
+1. **Navigate to the project directory:**
+    ```
+    cd <project_path>
+    ```
+
+2. **Create and activate a virtual environment:**
+
+    - **Setting up the environment:**
+        ```
+        python -m venv <virtual_environment_name>
+        ```
+
+    - **Activating the environment:**
+        ```
+        cd <virtual_environment_name>/Scripts
+        source activate
+        ```
+
+3. **Install the required packages:**
+    ```
+    pip install -r requirements.txt
+    ```
+
+4. **Run the script:**
+    ```
+    python main.py
+    ```
+
+## Code Workflow:
+
+### 1. **Database Connection and Data Fetching:**
+   
+- **Establishing Connection:**
+  The script uses the `sqlalchemy` library to create a connection (`engine`) to the SQLite database [data.sqlite](./data.sqlite).
+
+- **Creating a Session:**
+  A session is initialized in order to interact with the database.
+
+- **Executing the Query:**
+  - The query primarily involves three tables: `product_description`, `product`, and `manufacturer`. It also optionally includes data from `product_image` using a `LEFT JOIN`.
+  - `INNER JOIN` is used between `product_description` and `product` to fetch only products that adhere to the Google product data specification.
+  - Another `INNER JOIN` between `product` and `manufacturer` ensures the brand's presence for each product.
+  - `LEFT JOIN` with `product_image` is utilized to fetch optional additional images associated with each product, as well as the order in which they should appear.
+  - Multiple `<additional_image_link>` elements appear in one product in the xml because this is allowed in the google spec.
+  
+### 2. **Data Transformation:**
+
+- **Base URL Definition:**
+  The script uses a base URL (`https://butopea.com/`) to construct product URLs and image URLs.
+
+- **Data Structuring:**
+  Raw data from the database is transformed into a Python dictionary, ensuring that each product ID acts as a unique key. 
+  - Additional image links associated with each product are grouped together.
+  - The sort order is maintained for these additional image links.
+
+### 3. **XML Generation:**
+
+- **Building the XML Data Structure:**
+  The script structures the transformed data into a dictionary format compatible with the XML structure, where each product entry forms a `product` element under the root `feed` element.
+
+- **Converting Dictionary to XML:**
+  The `xmltodict` library is used to serialize the Python dictionary into an XML string.
+
+- **Saving to File:**
+  The XML string is then saved to a [feed.xml](./feed.xml) file. UTF-8 encoding is utilized to ensure special characters are correctly encoded.
+
+### Important Notes:
+
+- **Error Handling:** Throughout the script, error-handling mechanisms are in place, primarily using `try-except` blocks. This ensures that any potential issues, whether during data fetching or XML generation, are gracefully managed, and descriptive error messages are printed to the console.
+  
+- **Memory Management:** After processing, temporary data structures that are no longer needed (like the `products_dict` dictionary) are explicitly deleted to free up memory.
+
+---
+
+## Conclusion:
+
+The script provides an automated way to extract, transform, and save product data from an SQLite database into a structured XML file, adhering to google product data specification.
+
+
+## Author:
+
+- [Ayomide Ajayi](https://github.com/ayo-ajayi)
